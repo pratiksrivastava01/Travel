e2c7ddba9304a9290147cdabae82d68d6981ddf7
Pratik Srivastava <92784294+pratiksrivastava01@users.noreply.github.com>
2024-05-27 22:44:24 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import Footer from '@/components/Footer';
 
 export const metadata: Metadata = {
   title: 'Travel',
-  description: 'Travel UI/UX App for Camping',
+  description: 'Travel UI/UX App for Camping for Fun',
 }
 
 export default function RootLayout({
